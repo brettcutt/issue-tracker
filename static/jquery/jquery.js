4d99ee1703438779e78e381d558b168248d85fea
brett <brettcutt@outlook.com.au>
2018-11-29 11:46:39 +1030

    added html logic and css styling follow checkout update

--- static/jquery/jquery.js
+++ static/jquery/jquery.js
@@ -2,4 +2,11 @@ $(document).ready(function () {
     $('#menu-bars').click(function () {
         $("#collapse").toggle("blind", 1000);
     })
+
+    $('#repeated-upvote-btn').click(function() {
+        $('.upvote-btn-hidden').show('fade', 500)
+    })
+    $('#upvote-no').click(function() {
+        $('.upvote-btn-hidden').hide('fade', 500)
+    })
 })
