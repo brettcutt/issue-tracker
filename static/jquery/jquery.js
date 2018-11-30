45ad6f4ffaf9c1de5349be13ca2a0c059a6d576e
brett <brettcutt@outlook.com.au>
2018-11-30 23:01:33 +1030

    html layout and styling

--- static/jquery/jquery.js
+++ static/jquery/jquery.js
@@ -3,10 +3,49 @@ $(document).ready(function () {
         $("#collapse").toggle("blind", 1000);
     })
 
-    $('#repeated-upvote-btn').click(function() {
+    $('#repeated-upvote-btn').click(function () {
         $('.upvote-btn-hidden').show('fade', 500)
     })
-    $('#upvote-no').click(function() {
+    $('#upvote-no').click(function () {
         $('.upvote-btn-hidden').hide('fade', 500)
     })
+
+
+    $('#arrow-down').click(function () {
+        $('html, body').animate({
+            scrollTop: $("#charts").offset().top
+        }, 2000)
+    })
+
+    $(document).scroll(function () {
+        var y = $(this).scrollTop();
+        if (y > 0) {
+            $('#arrow-down').hide('fade', 1000);
+        } else {
+            $('#arrow-down').fadeIn();
+        }
+    });
+
+    $(document).scroll(function () {
+        var y = $(this).scrollTop();
+        if (y > 250) {
+
+            $('.test1').show('fade', 1000);
+            setTimeout(function () {
+
+                // your stuff here
+                $('.test2').show('fade', 1000);
+
+            }, 1000);
+            setTimeout(function () {
+
+                // your stuff here
+                $('.test3').show('fade', 1000);
+
+            }, 2000);
+
+        }
+    });
+
+
 })
