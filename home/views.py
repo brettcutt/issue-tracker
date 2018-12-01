01030a5295e5e891a6054b5e744caf3ce08111e3
brett <brettcutt@outlook.com.au>
2018-12-01 22:19:01 +1030

    Added graphs app, added three fuctions to render graphs

--- home/views.py
+++ home/views.py
@@ -1,6 +1,13 @@
+import pygal
 from django.shortcuts import render
+from bugs.models import Bugs
+from graphs.graphs import BugsPieChart, BugsDailyStatus, FeaturesPieChart
 
 
 def index(request):
     """ A view that displays the index page"""
-    return render(request, 'index.html')
+    bugs_status_chart = BugsPieChart()
+    features_status_chart = FeaturesPieChart()
+    bugs_daily_status = BugsDailyStatus()
+    return render(request, 'index.html', {'bugs_status_chart': bugs_status_chart, 'BugsDailyStatus':BugsDailyStatus,
+                                          'features_status_chart': features_status_chart})
